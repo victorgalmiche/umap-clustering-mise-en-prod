02b6f4a7d8ced0bfae1b9477fa69775e1415aafe
matheo <matheo.leroy@ensae.fr>
2026-03-28 22:36:34 +0000

    create the job that fit_trasnforms data with umap

--- src/__main__.py
+++ src/__main__.py
@@ -4,9 +4,15 @@ main
 
 from pathlib import Path
 from argparse import ArgumentParser, Namespace
-from config_loader import load_configs_from_files
-import logging 
+from settings.config_loader import load_config_from_file
+import logging
 import importlib
+from dotenv import load_dotenv
+import os
+import sys
+
+sys.path.append(str(Path(__file__).resolve().parent)) 
+# as we have not yet decided to make the project a package. 
 
 logger = logging.getLogger(Path(__file__).stem)
 
@@ -19,37 +25,46 @@ def parse_args() -> Namespace:
     """
 
     parser = ArgumentParser(description="Parse arguments from CLI")
-    parser.add_argument("configs", nargs="+", help="Config files for a job")
-    parser.add_argument(
-        "-e",
-        "--extras",
-        nargs="*",
-        default=[],
-        help="Config strings for the job, in JSON like format"
-    )
+    parser.add_argument("config", nargs="+", help="Config files for a job")
     
-    return parser.parse_args()
+    return parser.parse_args() 
 
 
-def logger_configure():
-    pass
+def load_config(list_config_file):
+
+    CONF_DIR = Path(__file__).resolve().parent.parent / "config" / os.getenv("ENV")
+    
+    if not CONF_DIR.exists():
+        raise FileNotFoundError(f"Config directory not found: {CONF_DIR}")
+    
+    path = CONF_DIR / list_config_file[0]
+
+    return load_config_from_file(path)   
 
 
 def main() -> None:
     """Run the desired application"""
 
+    load_dotenv()
+
+    logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
     args = parse_args()
-    config_paths: list[str] = args.configs
+    list_config_file = args.config
 
-    file_config = config_loader.load_configs_from_files(config_paths)
-    extra_config = config_loader.load_configs_from_strings(args.extras)
+    job_config = load_config(list_config_file)
 
-    job_config = config
-    job_name = Path(config_paths[-1])  # .stem.replace("-", "_")
+    job_name = Path(list_config_file[-1]).stem 
 
-    # job_module = importlib.import_module(name=f"name_package.{job_name}")
+    job_module = importlib.import_module(name=f"application.{job_name}")
 
     logger.info(f"Launching job {job_name}...")
 
-    # job_module.job(config=job_config)
+    job_module.job(config=job_config)
 
+
+if __name__ == "__main__":
+    main()
---
+++ src/application/dim_reduction.py
@@ -0,0 +1,66 @@
+"""
+"""
+
+import pandas as pd
+import os
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_iris
+from sklearn.manifold import trustworthiness
+import logging
+from pathlib import Path
+import mlflow
+
+from umap_algo.umap_class import umap_mapping
+from adapter.mlflow_tracker import ExperimentTracker
+
+logger = logging.getLogger(Path(__file__).stem)
+
+
+def job(config):
+
+    # dataset = pd.read_parquet(config.path_dataset)
+    logger.info("Loading Data...")
+    dataset = load_iris().data
+
+    scaler = StandardScaler()
+    hyperparameters = config.hyperparameters
+    model = umap_mapping(
+        n_neighbors=hyperparameters.n_neighbors,
+        n_components=hyperparameters.n_components,
+        min_dist=hyperparameters.min_dist,
+        KNN_metric=hyperparameters.KNN_metric,
+        KNN_method=hyperparameters.KNN_method,
+    )
+    
+    logger.info("Fitting model...")
+    dataset_standardized = scaler.fit_transform(dataset)
+    dataset_transformed = model.fit_transform(dataset_standardized)
+
+    trust = trustworthiness(
+        dataset, 
+        dataset_transformed, 
+        n_neighbors=config.metrics.n_neighbors_trustworthiness
+    )
+
+    metrics = {"trustworthiness": trust}
+
+    experiment_tracker = ExperimentTracker(
+        experiment_name=config.experiment_name,
+        run_name=config.run_name,
+        run_tags={"user": os.getenv("USER"), "dataset": config.dataset_name},
+    )
+
+    logger.info("Logging metrics, params...")
+    with experiment_tracker.run():
+        experiment_tracker.log_metrics(metrics)
+        experiment_tracker.log_params(hyperparameters)
+    logger.info("End of the job...")
+
+
+
+    
+
+
+
+
+
