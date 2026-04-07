29614d48cc75f6070eb3a5a02614ab58f491d8d4
mateomorin <mateo.morin.perso@gmail.com>
2026-04-07 16:59:37 +0000

    Streamlit v 0.1 - basic config

---
+++ app/streamlit/website_utils.py
@@ -0,0 +1,50 @@
+import requests
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans, HDBSCAN
+
+
+def run_umap_api(
+    df: pd.DataFrame,
+    n_neighbors: int = 15,
+    n_components: int = 2,
+    min_dist: float = 0.1,
+    knn_metric: str = "euclidean",
+    knn_method: str = "approx"
+) -> np.ndarray:
+    """
+    Call the API to run umap.
+    """
+    # url = "http://0.0.0.0:8000/umap"
+    url = "https://umap-api-mmvs.lab.sspcloud.fr/umap"
+
+    csv_buffer = df.to_csv(index=False).encode()
+
+    files = {
+        "file": ("data.csv", csv_buffer, "text/csv")
+    }
+
+    data = {
+        "n_neighbors": n_neighbors,
+        "n_components": n_components,
+        "min_dist": min_dist,
+        "knn_metric": knn_metric,
+        "knn_method": knn_method,
+    }
+
+    response = requests.post(url, files=files, data=data)
+
+    if response.status_code != 200:
+        raise Exception(f"API error: {response.text}")
+
+    data = response.json()
+
+    return np.array(data["embedding"])
+
+
+def run_kmeans(X, n_clusters):
+    return KMeans(n_clusters).fit(X).labels_
+
+
+def run_hdbscan(X, min_cluster_size):
+    return HDBSCAN(min_cluster_size).fit(X).labels_
